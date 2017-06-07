1c809d9b3830f465fb2d000ab23694280eafdbcb
Prashant Bhargava <prashant@artifacia.com>
2017-06-07 16:54:40 +0530

    getting top india trends

--- twitter_trending_topics_api.py
+++ twitter_trending_topics_api.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from gevent import monkey
 
+import config as cc
+import twitter_helpers
+
+INDIA_WOEID = cc.INDIA_WOEID
+
 monkey.patch_all()
 
 twitter_trending_app = Flask(__name__)
@@ -10,7 +15,8 @@ socketio = SocketIO(twitter_trending_app)
 
 @twitter_trending_app.route('/')
 def index():
-    return render_template('index.html')
+    trends = twitter_helpers.get_trending_topics(INDIA_WOEID)
+    return render_template('index.html', trends=trends)
 
 
 if __name__ == '__main__':
